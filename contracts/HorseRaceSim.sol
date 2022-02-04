5d3582291a566490ed44e8fba6bc1df3016b3b1a
W_Dubery <warrendubery@protonmail.ch>
2022-02-04 16:19:48 +0000

    Add HorseRaceSim contract

---
+++ contracts/HorseRaceSim.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+ 
+/// @title Horse Race Simulation
+/// @dev Simulates a betting situation, 1 player vs 4 NPCs and pays out to the winner
+
+contract HorseRaceSim {
+
+// TO COMPLETE
+}
