fec3e425fa8094449b77b523c6ee922528742808
W_Dubery <warrendubery@protonmail.ch>
2022-02-09 10:37:27 +0000

    Add constructor and modifier

--- contracts/HorseRaceSim.sol
+++ contracts/HorseRaceSim.sol
@@ -5,8 +5,19 @@ pragma solidity ^0.8.0;
 /// @dev Simulates a betting situation, 1 player vs 4 NPCs and pays out to the winner
 
 contract HorseRaceSim {
-
+    
     // Creates empty list of players and empty value for 'admin'
     address payable[] public players;
     address public admin;
+    
+    constructor() {
+        // Makes contract's initializing account, 'admin'
+        admin = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        // Ensure only owner can call functions with this modifier
+        require(admin == msg.sender, "You are not the owner");
+        _;
+    }
 }
