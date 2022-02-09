825b59a7975ba8ab7a2e36d4bad05a5b4b6c81d8
W_Dubery <warrendubery@protonmail.ch>
2022-02-09 17:24:30 +0000

    Update Natspec

--- contracts/HorseRaceSim.sol
+++ contracts/HorseRaceSim.sol
@@ -1,8 +1,12 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
- 
-/// @title Horse Race Simulation
-/// @dev Simulates a betting situation, 1 player vs 4 NPCs and pays out to the winner
+
+/* 
+@title Horse Race Simulation
+@license GNU GPLv3
+@author Warren Dubery
+@dev Simulates a betting situation, 1 player vs 4 NPCs and pays out to the winner
+*/
 
 contract HorseRaceSim {
     
