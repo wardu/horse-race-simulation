df07d7b5ddaa71f75db4866343fa7c3f8cccdff6
W_Dubery <warrendubery@protonmail.ch>
2022-02-11 17:15:05 +0000

    Add receive function

--- contracts/HorseRaceSim.sol
+++ contracts/HorseRaceSim.sol
@@ -1,12 +1,8 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
-
-/* 
-@title Horse Race Simulation
-@license GNU GPLv3
-@author Warren Dubery
-@dev Simulates a betting situation, 1 player vs 4 NPCs and pays out to the winner
-*/
+ 
+/// @title Horse Race Simulation
+/// @dev Simulates a betting situation, 1 player vs 4 NPCs and pays out to the winner
 
 contract HorseRaceSim {
     
@@ -17,11 +13,21 @@ contract HorseRaceSim {
     constructor() {
         // Makes contract's initializing account, 'admin'
         admin = msg.sender;
+        //automatically adds admin on deployment
+        // players.push(payable(admin));
     }
     
     modifier onlyOwner() {
         // Ensure only owner can call functions with this modifier
         require(admin == msg.sender, "Only the owner can call that function!");
         _;
+    }
+    
+    /// Receive function allows contract to accept 'bets' of 0.1 Ether
+    receive() external payable {
+        require(msg.value == 0.1 ether , "Bets placed must be of size 0.1 ETH");
+        
+        // Add sender's address to players list
+        players.push(payable(msg.sender));
     }
 }
